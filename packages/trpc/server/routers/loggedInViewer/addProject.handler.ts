3b97fe8f01d2d5541f292371b052fff96682f739
Danish Abdullah <dev@danishabdullah.com>
2023-10-16 14:41:55 +0200

    added the ability to add the many to many automatically

--- packages/trpc/server/routers/loggedInViewer/addProject.handler.ts
+++ packages/trpc/server/routers/loggedInViewer/addProject.handler.ts
@@ -1,15 +1,36 @@
+import type { GetServerSidePropsContext, NextApiResponse } from "next";
+
 import { prisma } from "@calcom/prisma";
+import type { TrpcSessionUser } from "@calcom/trpc/server/trpc";
 
 import type { TProjectAddSchema } from "./project.schema";
 
 type AddProject = {
+  ctx: {
+    user: NonNullable<TrpcSessionUser>;
+    res?: NextApiResponse | GetServerSidePropsContext["res"];
+  };
   input: TProjectAddSchema;
 };
 
-export const addProjectHandler = async ({ input }: AddProject) => {
-  const res = await prisma.project.create({
-    data: input,
+export const addProjectHandler = async ({ ctx, input }: AddProject) => {
+  const { user } = ctx;
+  console.log("handler", user);
+  console.log("post input", input);
+
+  const addProject = await prisma.user.update({
+    where: {
+      id: ctx.user.id,
+    },
+    data: {
+      projects: {
+        create: {
+          ...input,
+          updatedAt: new Date(),
+        },
+      },
+    },
   });
 
-  return { ...res };
+  return { ...addProject };
 };
--- packages/trpc/server/routers/loggedInViewer/addPublication.handler.ts
+++ packages/trpc/server/routers/loggedInViewer/addPublication.handler.ts
@@ -1,15 +1,36 @@
+import type { GetServerSidePropsContext, NextApiResponse } from "next";
+
 import { prisma } from "@calcom/prisma";
+import type { TrpcSessionUser } from "@calcom/trpc/server/trpc";
 
 import type { TPublicationAddSchema } from "./publication.schema";
 
 type AddPublication = {
+  ctx: {
+    user: NonNullable<TrpcSessionUser>;
+    res?: NextApiResponse | GetServerSidePropsContext["res"];
+  };
   input: TPublicationAddSchema;
 };
 
-export const addPublicationHandler = async ({ input }: AddPublication) => {
-  const res = await prisma.publication.create({
-    data: input,
+export const addPublicationHandler = async ({ ctx, input }: AddPublication) => {
+  const { user } = ctx;
+  console.log("handler", user);
+  console.log("post input", input);
+
+  const addPublication = await prisma.user.update({
+    where: {
+      id: ctx.user.id,
+    },
+    data: {
+      publications: {
+        create: {
+          ...input,
+          updatedAt: new Date(),
+        },
+      },
+    },
   });
 
-  return { ...res };
+  return { ...addPublication };
 };
