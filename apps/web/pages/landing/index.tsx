61886d1262f7eee6d18beded97e8f5140fa96eb5
Valery Sibikovsky <combdn@gmail.com>
2023-10-17 19:32:14 +0100

    update: hero and footer responsiveness

--- apps/web/pages/landing/index.tsx
+++ apps/web/pages/landing/index.tsx
@@ -107,10 +107,10 @@ const LandingPage = () => {
         <main className="flex w-full flex-col items-center">
           {/* Hero */}
           <div
-            className="flex h-[735px] w-full flex-col items-center bg-[url(/borg/images/landing/cellular-automata-bg.png)] bg-left-top"
+            className="flex w-full flex-col items-center bg-[url(/borg/images/landing/cellular-automata-bg.png)] bg-left-top px-[5%] pb-[172px] pt-[164px]"
             style={{ imageRendering: "pixelated" }}>
-            <hgroup className="mt-[164px] max-w-xl text-center">
-              <h1 className="text-landing-4xl mb-2 font-extrabold leading-[60px]">
+            <hgroup className="max-w-xl text-center">
+              <h1 className="text-landing-3xl md:text-landing-4xl mb-2 font-extrabold leading-[60px]">
                 A{" "}
                 <span className="relative z-10 after:absolute after:bottom-[10px] after:left-0 after:z-[-1] after:block after:h-[12px] after:w-full after:bg-green-300 after:content-['']">
                   Better
@@ -290,10 +290,18 @@ const LandingPage = () => {
           </div>
 
           {/* Footer */}
-          <footer className="flex max-w-7xl flex-col gap-6 py-[5%] pb-4 pt-14">
-            <div className="grid w-full grid-cols-12 gap-12">
-              <Image src="/borg/borg-id-logo.svg" width={88} height={30} className="col-span-9" />
-              <div className="col-span-2 flex flex-col gap-4">
+          <footer className="flex w-full max-w-7xl flex-col gap-10 px-[5%] pb-4 pt-10 md:gap-6 md:pt-14">
+            <div
+              className="grid w-full grid-flow-dense gap-y-10 md:gap-y-0"
+              style={{ gridTemplateColumns: "repeat(12, minmax(0, 1fr))" }}>
+              <Image
+                src="/borg/borg-id-logo.svg"
+                width={88}
+                height={30}
+                className="col-span-6 md:col-span-7 lg:col-span-9"
+                alt="Borg.id logo"
+              />
+              <div className="col-span-12 flex flex-row flex-wrap justify-center gap-x-6 gap-y-0 md:col-span-3 md:flex-col md:gap-3 lg:col-span-2">
                 <Button variant="link" size="sm" className="w-max px-0">
                   Privacy Policy
                 </Button>
@@ -301,13 +309,13 @@ const LandingPage = () => {
                   Cookie Policy
                 </Button>
                 <Button variant="link" size="sm" className="w-max px-0">
-                  Terms & Conditions
-                </Button>
-                <Button variant="link" size="sm" className="w-max px-0">
                   Impressum
                 </Button>
+                <Button variant="link" size="sm" className="w-max px-0">
+                  Terms & Conditions
+                </Button>
               </div>
-              <div className="col-span-1 flex h-9 items-center justify-end gap-6">
+              <div className="col-span-6 flex h-9 items-center justify-end gap-6 md:col-span-2 lg:col-span-1">
                 <Twitter size={16} />
                 <Envelope size={16} />
               </div>
