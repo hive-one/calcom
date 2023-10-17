81ecf0673bd7d6817315a281f3f6d20a1ba2649f
Danish Abdullah <dev@danishabdullah.com>
2023-10-17 09:51:09 +0200

    typofix

--- packages/trpc/server/routers/loggedInViewer/addVideo.schema.ts
+++ packages/trpc/server/routers/loggedInViewer/addVideo.schema.ts
@@ -1,9 +1,9 @@
 import { z } from "zod";
 
-export const ZVideoInputSchema = z.object({
+export const ZVideoAddInputSchema = z.object({
   title: z.string(),
   url: z.string(),
   description: z.string(),
 });
 
-export type TVideoInputSchema = z.infer<typeof ZVideoInputSchema>;
+export type TVideoAddInputSchema = z.infer<typeof ZVideoAddInputSchema>;
