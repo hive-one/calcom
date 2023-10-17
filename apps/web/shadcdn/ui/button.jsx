23796b621db406821ee5f2df1c6bebceaad22011
Valery Sibikovsky <combdn@gmail.com>
2023-10-17 13:19:57 +0100

    add: landing (not responsive, no callouts)

--- apps/web/shadcdn/ui/button.jsx
+++ apps/web/shadcdn/ui/button.jsx
@@ -4,7 +4,7 @@ import * as React from "react";
 import { cn } from "shadcdn/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center rounded-md text-sm ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center rounded-md ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
@@ -17,9 +17,10 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md py-2 px-8",
+        default: "h-10 px-4 py-2 text-sm",
+        sm: "h-9 rounded-md px-3 text-sm",
+        lg: "h-11 rounded-md py-2 px-8 text-sm",
+        landing: "h-12 rounded-md px-6 py-3 text-lg",
         icon: "h-10 w-10",
       },
     },
